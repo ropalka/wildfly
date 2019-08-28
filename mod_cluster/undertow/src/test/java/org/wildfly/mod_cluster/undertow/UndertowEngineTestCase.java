c1bf94c83b5e815bde81bbc00fa8054489ccef21
Richard Opálka <opalka.richard@gmail.com>
2019-08-28 23:50:15 +0200

    [WFLY-12945] Refactoring ListenerService, Http(s)ListenerService & AjpListenerService to new MSC value API

--- mod_cluster/undertow/src/test/java/org/wildfly/mod_cluster/undertow/UndertowEngineTestCase.java
+++ mod_cluster/undertow/src/test/java/org/wildfly/mod_cluster/undertow/UndertowEngineTestCase.java
@@ -52,7 +52,7 @@ public class UndertowEngineTestCase {
     private final String hostName = "default-host";
     private final String route = "route";
     private final Host host = new Host(this.hostName, Collections.emptyList(), "ROOT.war", StatusCodes.NOT_FOUND, false);
-    private final HttpsListenerService listener = new HttpsListenerService(PathAddress.pathAddress(Constants.HTTPS_LISTENER, "default"), "https", OptionMap.EMPTY, null, OptionMap.EMPTY, false);
+    private final HttpsListenerService listener = new HttpsListenerService(null, PathAddress.pathAddress(Constants.HTTPS_LISTENER, "default"), "https", OptionMap.EMPTY, null, OptionMap.EMPTY, false);
 
     private final UndertowService service = new TestUndertowService("default-container", this.serverName, this.hostName, this.route, this.server);
     private final Server server = new TestServer(this.serverName, this.hostName, this.service, this.host, this.listener);
